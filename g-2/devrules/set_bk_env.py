f7b5cd1007626eee61f32e95282d416e7863431c
Aaron Fienberg <atfienberg@aarons-mbp.dhcp.fnal.gov>
2017-04-27 15:58:51 -0500

    fixed set_bk_env

--- g-2/devrules/set_bk_env.py
+++ g-2/devrules/set_bk_env.py
@@ -24,9 +24,9 @@ def main():
         sys.exit(0)
         
     bk = serial.Serial('/dev/' + sys.argv[1], 4800, timeout=0.5)
-    bk.write('*IDN?\n')
-    resp = read_response(bk)
     bk.write('SOUR:CURR 0.005\n')
+    resp = read_response(bk)
+    bk.write('*IDN?\n')
     resp = read_response(bk)
     if resp[:-1] in SERIALS:      
         print('bknum=%i' % (SERIALS.index(resp[:-1]) + 1))
