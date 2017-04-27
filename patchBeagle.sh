657fb5b1e419df21ccf9b08906760320e25a00f5
Aaron Fienberg <atfienberg@aarons-mbp.dhcp.fnal.gov>
2017-04-27 16:18:46 -0500

    fixed patch beagle

--- patchBeagle.sh
+++ patchBeagle.sh
@@ -11,5 +11,7 @@ scp g-2/beagle-code/sipmspi.py root@$1:/home/g-2/beagle-code/sipmspi.py
 scp g-2/beagle-code/spiworker.py root@$1:/home/g-2/beagle-code/spiworker.py
 scp g-2/beaglecron root@$1:/etc/cron.d/beaglecron
 scp g-2/beagle-code/bkworker.py root@$1:/home/g-2/beagle-code/bkworker.py
+scp g-2/beagle-code/jmuworker.py root@$1:/home/g-2/beagle-code/jmuworker.py
+scp g-2/beagle-code/startMasterWorkers.sh root@$1:/home/g-2/beagle-code/startMasterWorkers.sh
 scp g-2/devrules/set_bk_env.py root@$1:/home/g-2/devrules/set_bk_env.py
 ssh root@$1 "date --set $year; date --set $time; hwclock --systohc; chmod u+x /home/g-2/devrules/set_bk_env.py; cd /home/g-2/beagle-code; touch src/*; make"
